0ae8be9b0d08d4f1a8c98e02dcdb17f4aaab1dea
zukako0930 <kou.zukako@gmail.com>
2017-10-06 18:07:59 +0900

    ツイート数といいね数の偏差値を出力+引数をscreen_nameのみに統一

--- motemote/api/np.py
+++ motemote/api/np.py
@@ -12,56 +12,63 @@ AS = 'LQ8xXBTTN8F8CHQv9oDAqsGJFeexdnFf2DFzn3EzGH2L8'
 
 twitter = OAuth1Session(CK, CS, AT, AS)
 
-def get_usid(scname):
-    url = "https://api.twitter.com/1.1/users/show.json"
-    # とくにパラメータは無い
-    params = { "screen_name" : scname}
-    # OAuth で GET
-    twitter = OAuth1Session(CK, CS, AT, AS)
-    req = twitter.get(url, params = params)
-    n = 0
-    if req.status_code == 200:
-        timeline2 = json.loads(req.text)
-        print(timeline2["id"])
-        return timeline2["id"]
-    else:
-        return 0
-
-def get_tweet(usid):
+def get_tweet(screen_name):
     tweet_list=[]
-    # タイムライン取得用のURL
+    favolist=[]
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=300"
-    # とくにパラメータは無い
-    params = { "user_id" : usid}
-    # OAuth で GET
+    params = {"screen_name" : screen_name}
     twitter = OAuth1Session(CK, CS, AT, AS)
     req = twitter.get(url, params = params)
     n = 0
-    # print(users["id_str"])
-    if req.status_code == 200:
-        # レスポンスはJSON形式なので parse する
+    if req.status_code != 200:
+        return {
+            'status': req.status_code,
+            'reason': req.reason
+        }
+    else:
         timeline = json.loads(req.text)
-        # 各ツイートの本文を表示
         print(len(timeline))
         for tweet in timeline:
             if '@' not in tweet["text"]:
                 tweet_list.append(tweet["text"])
-                #print(tweet["text"])
+                favolist.append(tweet["favorite_count"])
             if len(tweet_list)==100:
                 break
-        return tweet_list
-#         print(tweet_list)
-    else:
-        # エラーの場合
-        print ("Error: %d" % req.status_code)
-        return 0
+        favo = sum(favolist)/len(favolist)
+        favo_ave = 1.76596070695
+        favo_std = 5.3013228418991245
+        favo_dev = calc_dev(favo,favo_ave,favo_std)
+
+        nega_posi_score = calc_score(tweet_list)
+        nega_posi_ave = -0.3003982968868934
+        nega_posi_std = 0.06567610108001085
+        nega_posi_dev = calc_dev(nega_posi_score,nega_posi_ave,nega_posi_std)
+        return  {
+            "favo_dev": favo_dev,
+            "nega_posi_dev": nega_posi_dev,
+        }
+
+def get_tweetCount_dev(screen_name):
+    url = "https://api.twitter.com/1.1/users/show.json"
+#     url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json"
+    favolist = []
+    params = {
+        "screen_name": screen_name,
+        "include_entities": True,
+    }
+    req = twitter.get(url, params=params)
+    result_tC = json.loads(req.text)
+    tweet_count = result_tC['statuses_count']
+    tweet_count_ave = 2969.449438202247
+    tweet_count_std = 2765.3002952842094
+    tweet_count_dev = calc_dev(tweet_count,tweet_count_ave,tweet_count_std)
+    return tweet_count_dev
 
 def calc_score(tweet_list):
     score=0
     sum_score=0
     sc_list=[]
     for idx,tweet in enumerate(tweet_list):
-#         print(idx)
         tw1=nega_posi.JudgeClass(tweet)
         tw1.make_wlist()
         score_dict=tw1.make_dict()
@@ -78,39 +85,25 @@ def calc_score(tweet_list):
         min_idx=search_min_idx(sc_list)
         for i,idx in enumerate(min_idx):
             print("negative "+str(i+1)+"位: "+str(tweet_list[idx]))
-        print(max_idx)
-        print(min_idx)
         print("全体平均"+str(sum_score/len(tweet_list)))
         return sum_score/len(tweet_list)
 
 def search_max_idx(sc_list):
     my_array=np.array(sc_list)
-    #探す対象リスト:my_arrayはnumpy
-    #例:上位3件
     K = 3
-    # ソートはされていない上位k件のインデックス
     unsorted_max_indices = np.argpartition(-my_array, K)[:K]
-    # 上位k件の値
     y = my_array[unsorted_max_indices]
-    # 大きい順にソートし、インデックスを取得
     max_indices = np.argsort(-y)
-    # 類似度上位k件のインデックス
     max_k_indices = unsorted_max_indices[max_indices]
     return max_k_indices
 
 def search_min_idx(sc_list):
     my_array=np.array(sc_list)
     my_array=my_array*(-1)
-    #探す対象リスト:my_arrayはnump
-    #例:上位3件
     K = 3
-    # ソートはされていない上位k件のインデックス
     unsorted_max_indices = np.argpartition(-my_array, K)[:K]
-    # 上位k件の値
     y = my_array[unsorted_max_indices]
-    # 大きい順にソートし、インデックスを取得
     max_indices = np.argsort(-y)
-    # 類似度上位k件のインデックス
     max_k_indices = unsorted_max_indices[max_indices]
     return max_k_indices
 
--- motemote/coluclate_mote/views.py
+++ motemote/coluclate_mote/views.py
@@ -5,60 +5,44 @@ from django.contrib import messages
 from api import mote
 from api import nega_posi,np
 from coluclate_mote.form import TwitterAcountForm
-
+import json
 
 def index(request):
     content = {}
-
     form = TwitterAcountForm(auto_id=False)
     content['form'] = form
     return render(request, 'coluclate_mote/index.html', content)
 
-
 def result(request):
     content = {}
-
     if request.method == 'POST':
         form = TwitterAcountForm(request.POST)
-
         if not form.is_valid():
             for error in form.errors.values():
                 messages.error(request, error)
             return redirect('/coluclate_mote/index#top_form')
-
         screen_name = form.data['screen_name'].replace('@','')
         content['screen_name'] = screen_name
         return render(request, 'coluclate_mote/result.html',content)
     else:
         raise Http404
 
-
 def call_mote_api(request):
-    import json
-
     if request.method == 'POST':
         params = json.loads(request.body.decode('utf-8'))
         screen_name = params['screen_name']
         result = mote.calc_mote(screen_name)
-        ave=-0.3003982968868934
-        std=0.06567610108001085
-        usid=np.get_usid(screen_name)
-        tweet_list=[]
-        tweet_list=np.get_tweet(usid)
-        #print(len(tweet_list))
-        np_score=np.calc_score(tweet_list)
-        dev=np.calc_dev(np_score,ave,std)
-
-        print("ポジティブ偏差値は : "+str(dev))
-
+        result_np = np.get_tweet(screen_name)
+        tweet_count_dev = np.get_tweetCount_dev(screen_name)
+        print("ポジティブ偏差値は : "+ str(result_np['nega_posi_dev']))
+        print("いいね数偏差値は : " + str(result_np['favo_dev']))
+        print("tweet数偏差値は : "+str(tweet_count_dev))
         score = result['score']
         result['score'] = int(score * 100)
-
         response = json.dumps({'result': result})
         return HttpResponse(response, content_type='application/json')
     else:
         raise Http404
-
 
 def how(request):
     return render(request, 'coluclate_mote/how.html')
