1899d3fa9ba3dcc66140bca27c768a76c965ee4a
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2017-12-01 11:40:48 +0900

    changed getTweet method to using tweepy

--- motemote/api/np.py
+++ motemote/api/np.py
@@ -1,4 +1,5 @@
 from api import nega_posi
+from api import twitter_getTweets as tg
 import numpy as np
 from api import twitter_api
 from requests_oauthlib import OAuth1Session
@@ -10,46 +11,25 @@ CS = '5jPoQ5kQvMJFDYRNE8bQ4rHuds4xJqhvgNJM4awaE8'
 AT = '333312023-6dTniMxvwlQG8bATKNYWBXaQkftz9t4ZjRBt7BWk'
 AS = 'LQ8xXBTTN8F8CHQv9oDAqsGJFeexdnFf2DFzn3EzGH2L8'
 
-twitter = OAuth1Session(CK, CS, AT, AS)
 
 def get_tweet(screen_name):
-    tweet_list=[]
-    favolist=[]
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=300"
-    params = {"screen_name" : screen_name}
-    twitter = OAuth1Session(CK, CS, AT, AS)
-    req = twitter.get(url, params = params)
-    n = 0
-    if req.status_code != 200:
-        return {
-            'status': req.status_code,
-            'reason': req.reason
-        }
-    else:
-        timeline = json.loads(req.text)
-        print(len(timeline))
-        for tweet in timeline:
-            if '@' not in tweet["text"]:
-                tweet_list.append(tweet["text"])
-                favolist.append(tweet["favorite_count"])
-            if len(tweet_list)==100:
-                break
-        if len(tweet_list) == 0:
-            favo = 0
-        else :
-            favo = sum(favolist)/len(favolist)
-        favo_ave = 1.76596070695
-        favo_std = 5.3013228418991245
-        favo_dev = calc_dev(favo,favo_ave,favo_std)
+    twGetter = tg.TweetGetter(CK, CS, AT, AS)
+    tweet_list, favolist = \
+            twGetter.getTimelineTextsFavos(screen_name, limit=100)
 
-        nega_posi_score = calc_score(tweet_list)
-        nega_posi_ave = -0.3003982968868934
-        nega_posi_std = 0.06567610108001085
-        nega_posi_dev = calc_dev(nega_posi_score,nega_posi_ave,nega_posi_std)
-        return  {
-            "favo_dev": favo_dev,
-            "nega_posi_dev": nega_posi_dev,
-        }
+    favo = sum(favolist)/len(favolist)
+    favo_ave = 1.76596070695
+    favo_std = 5.3013228418991245
+    favo_dev = calc_dev(favo,favo_ave,favo_std)
+
+    nega_posi_score = calc_score(tweet_list)
+    nega_posi_ave = -0.3003982968868934
+    nega_posi_std = 0.06567610108001085
+    nega_posi_dev = calc_dev(nega_posi_score,nega_posi_ave,nega_posi_std)
+    return  {
+        "favo_dev": favo_dev,
+        "nega_posi_dev": nega_posi_dev,
+    }
 
 def get_tweetCount_dev(screen_name):
     url = "https://api.twitter.com/1.1/users/show.json"
